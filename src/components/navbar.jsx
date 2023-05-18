d68faf66142af7235f9fdfdbb0ea1bf346345d55
Ali Shan <optimist.alishan@gmail.com>
2023-05-18 19:22:05 +0500

    first my F

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import './header.css'
 export default function NavbarTop() {
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const navbar = document.getElementById('navbar');
+          
+            if (window.scrollY > 0) {
+              navbar.classList.add('scrolled');
+            } else {
+              navbar.classList.remove('scrolled');
+            }
+          };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        // Clean up the event listener on component unmount
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+    
+
     return(
-        <nav className="navbar sticky-top navbar-expand-lg text-light py-4 border-b bg-body-tertiary">
+        <nav id='navbar' className="navbar fixed-top navbar-expand-lg text-light py-4 border-b bg-body-tertiary">
         <div className="container">
             <a className="navbar-brand text-light" href="#">&lt;code/&gt;</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
