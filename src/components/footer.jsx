bf56156f1144784e84ecfea2fb035d405db7cd7e
alishan <optimist.alishan@gmail.com>
2023-07-16 04:37:33 +0500

    github user

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,25 @@
+// import { useEffect, useState } from 'react';
 import '../components/footer.css'
+// import axios from 'axios';
 
 
 export default function Footer() {
+    // const [userData, setUserData] = useState(null);
+    
+    // useEffect(() => {
+    //     const fetchUserData = async () => {
+    //       try {
+    //         const response = await axios.get('https://api.github.com/users/al3sha9');
+    //         setUserData(response.data);
+    //       } catch (error) {
+    //         console.error('Error fetching user data:', error);
+    //       }
+    //     };
+      
+    //     fetchUserData();
+    //   }, []);
+      
+
     return (
         <section className="projects" id="projects">
             <div className="container">
@@ -10,6 +28,16 @@ export default function Footer() {
                         <div className="foot text-center mt-4 ">
                             <p className='pt-3 toper-para pb-4'>Made with &#9825; at techalphalogix |  ©️ all rights reserved  </p>
                         </div>
+                        {/* <div>
+                            {userData ? (
+                            <div>
+                                <h2>{userData.name}</h2>
+                                <p>{userData.bio}</p>
+                            </div>
+                            ) : (
+                            <p>Loading user data...</p>
+                            )}
+                        </div> */}
                     </div>
                 </div>
             </div>
