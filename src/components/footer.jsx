fb3ae206099df66d7a10c3c9545ae3c053a805a9
alishan <optimist.alishan@gmail.com>
2023-07-17 19:04:59 +0500

    end

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -26,7 +26,7 @@ export default function Footer() {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="foot text-center mt-4 ">
-                            <p className='pt-3 toper-para pb-4'>Made with &#9825; at techalphalogix |  ©️ all rights reserved  </p>
+                            <p className='pt-3 toper-para pb-4'>Made with &#9825; at techalphalogix by Ali Shan |  ©️ all rights reserved  </p>
                         </div>
                         {/* <div>
                             {userData ? (
